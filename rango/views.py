10ca0e66e91189de1870942481f3968914284717
Trine Olsen <2420036o@student.gla.ac.uk>
2019-01-14 16:44:57 +0100

    Chapter 3 done

--- rango/views.py
+++ rango/views.py
@@ -4,4 +4,10 @@ from django.shortcuts import render #remove?
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("Rango says hey there partner! Go to about page: <br/> <a href='/rango/about/'>About</a> ")
+    # <br/> new line
+    # <a href='/rango/about/'>About</a> link to rango/about. The link is shown as About
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. Go back: <a href='/rango/'>Index</a> ")
