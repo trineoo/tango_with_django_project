59ca2bd6adab75d6937d1c46cdeb5e49900ac370
Trine Olsen <2420036o@student.gla.ac.uk>
2019-01-24 12:47:40 +0100

    Finish chapter 8

--- rango/views.py
+++ rango/views.py
@@ -23,8 +23,12 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints 'AnonymousUser'
+    print(request.user)
     #return HttpResponse("Rango says here is the about page. Go back: <a href='/rango/'>index</a> ")
-    return render(request, 'rango/about.html')
+    return render(request, 'rango/about.html') #pass through an empty dic
 
 
 def show_category(request, category_name_slug):
